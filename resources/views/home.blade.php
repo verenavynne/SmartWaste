ed29c43948f9622de38cd7e263e2006aa95f96d2
verena.vynne@gmail.com <verena.vynne@gmail.com>
2024-10-01 12:47:05 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="px-5 py-2 d-flex justify-content-between align-items-center shadow-sm">
+        <h1>Smart Waste</h1>
+
+        <div class="d-flex gap-3 ">
+            <a href="/" class="fs-5">Home</a>
+            <a href="/" class="fs-5">Drop In</a>
+            <a href="/" class="fs-5">Mission</a>
+            <a href="/" class="fs-5">About Us</a>
+        </div>
+        <div class="md">
+            <i class="fa-solid fa-user fa-lg "></i>
+        </div>
+    </div>
+
+    <img src="assets/image/banner.png" alt="Banner" class="object-fit-cover" style="width: 100%; height: 400px">
+
+    <div class="container pt-5">
+    <div class="row ">
+        <div class="bg-danger col-4">
+            Sampah B3
+            <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eos, ex asperiores optio accusamus voluptatem quo suscipit minima modi iusto corporis temporibus ipsum dicta doloribus iure cumque, vero repudiandae maiores!
+           </p> 
+        </div>
+
+        <div class="bg-success col-4">
+            Sampah Organik
+
+           <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eos, ex asperiores optio accusamus voluptatem quo suscipit minima modi iusto corporis temporibus ipsum dicta doloribus iure cumque, vero repudiandae maiores!
+           </p> 
+        </div>
+
+        <div class="bg-warning col-4">
+            Sampak Anorganik
+            <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eos, ex asperiores optio accusamus voluptatem quo suscipit minima modi iusto corporis temporibus ipsum dicta doloribus iure cumque, vero repudiandae maiores!
+           </p> 
+        </div>
+    </div>
+    </div>
+
+    <div>
+
+    </div>
+
+</body>
+</html>
